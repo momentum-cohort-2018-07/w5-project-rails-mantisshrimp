3d3763530c5442ff071b0b29cfe92aee2f40d60a
lifeprolific <thegregoman@gmail.com>
2018-08-31 10:54:02 -0400

    added session controller actions

--- breddit/app/controllers/session_controller.rb
+++ breddit/app/controllers/session_controller.rb
@@ -1,2 +1,17 @@
 class SessionController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+    end
+    redirect_to posts_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to posts_path
+  end
 end
