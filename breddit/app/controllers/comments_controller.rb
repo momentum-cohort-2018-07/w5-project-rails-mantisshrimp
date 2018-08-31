029010502dacac32a9f23f92ef1534f252dab105
Brandon Pitzen <bmpitzen@icloud.com>
2018-08-31 11:16:18 -0400

    Feature added. Comments working

--- breddit/app/controllers/comments_controller.rb
+++ breddit/app/controllers/comments_controller.rb
@@ -1,4 +1,27 @@
 class CommentsController < ApplicationController
   def new
+    @comment = Comment.new
   end
+
+  def index
+    @comments = Comment.all
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to root_path, notice: 'Comment was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:comment_text, :user_id)
+    end
 end
