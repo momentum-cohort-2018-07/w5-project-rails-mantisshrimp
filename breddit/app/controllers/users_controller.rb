08f579bcb728877802641f1fe28748eb67e5d9b8
lifeprolific <thegregoman@gmail.com>
2018-08-31 10:14:22 -0400

    added user controller actions

--- breddit/app/controllers/users_controller.rb
+++ breddit/app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+  def register
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:username, :password))
+    if @user.save
+      redirect_to posts_path
+    else
+      render 'register'
+    end
+  end
+
 end
