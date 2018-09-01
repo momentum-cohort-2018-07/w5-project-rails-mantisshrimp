532c619cc2ed640cbdaae8e777bea8c290344f11
lifeprolific <thegregoman@gmail.com>
2018-09-01 13:07:43 -0400

    login check and hidden params for new comment

--- breddit/app/controllers/comments_controller.rb
+++ breddit/app/controllers/comments_controller.rb
@@ -12,14 +12,17 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = Comment.new(comment_params)
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to root_path, notice: 'Comment was successfully created.' }
-      else
-        format.html { render :new }
+    if current_user
+      @comment = Comment.new(comment_params)
+      respond_to do |format|
+        if @comment.save
+          format.html { redirect_to root_path, notice: 'Comment was successfully created.' }
+        else
+          format.html { render :new }
+        end
       end
+    else
+      redirect_to posts_path
     end
   end
 
