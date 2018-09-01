24e745eedb89fea6cea80388a4a87dabe7859a46
lifeprolific <thegregoman@gmail.com>
2018-09-01 13:47:32 -0400

    added new vote on post show and needed actions

--- breddit/app/controllers/votes_controller.rb
+++ breddit/app/controllers/votes_controller.rb
@@ -1,2 +1,10 @@
 class VotesController < ApplicationController
+  def create
+    @vote = Vote.new(params.require(:vote).permit(:vote_value, :user_id, :post_id))
+    @vote.save
+    redirect_to post_path(@vote.post_id)
+  end
+
+  def update
+  end
 end
